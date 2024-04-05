33d7294f2afe1e1ef18e28e3043f4389a3b103b1
Your Name <you@example.com>
2024-04-06 03:01:14 +1000

    updated the target to espcamlcd and disab;ed tracking for this branch

--- src/app_httpd.cpp
+++ src/app_httpd.cpp
@@ -29,8 +29,8 @@
 // Face Recognition takes upward from 15 seconds per frame on chips other than ESP32S3
 // Makes no sense to have it enabled for them
 #if CONFIG_IDF_TARGET_ESP32S3
-#define CONFIG_ESP_FACE_RECOGNITION_ENABLED 1
-#define CONFIG_ESP_FACE_DETECT_ENABLED 1
+#define CONFIG_ESP_FACE_RECOGNITION_ENABLED 0
+#define CONFIG_ESP_FACE_DETECT_ENABLED 0
 #else
 #define CONFIG_ESP_FACE_RECOGNITION_ENABLED 0
 #define CONFIG_ESP_FACE_DETECT_ENABLED 0
