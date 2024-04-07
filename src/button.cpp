326d5f45e53dca88d418d99561b3222483285758
DPelevin7 <126933058+DPelevin7@users.noreply.github.com>
2024-04-07 10:29:08 +1000

    No Serial Prints
    - Idk why this is happening
    - Changed the GPIO pin number to 1

--- src/button.cpp
+++ src/button.cpp
@@ -8,11 +8,17 @@ void audio_button::init() {
 
 
 /*! @brief Checks if the audio needs to be activated. */
-void audio_button::audio_activation(bool activated) {
+void audio_button::audio_activation() {
     check_buttons();
     if(!activated) {
         if(buttons[AUDIO_BUTTON_NUMBER]) {
             // Start Sending Audio
+            Serial.println('Print Audio');
+        }
+    } else {
+        if(!buttons[AUDIO_BUTTON_NUMBER]) {
+            // Second INTERRUPT to Stop Sending Audio
+            Serial.println('Stop Audio');
         }
     }
 }
@@ -20,7 +26,10 @@ void audio_button::audio_activation(bool activated) {
 
 /*! @brief Checks the button GPIO to determine which button is being pressed. */
 void audio_button::check_buttons() {
+    Serial.println('Error?');
     int voltage_out = analogRead(BUTTON_GPIO_PIN);
+    Serial.print('Button Output: ');
+    Serial.print('\t');
     Serial.println(voltage_out);
     for(int i = 0; i < 4; i++) {
         buttons[i] = abs(voltage_out - voltages[i]) < 15;
--- src/button.h
+++ src/button.h
@@ -5,17 +5,18 @@
 
 #define AUDIO_BUTTON_NUMBER 0
 #define BUTTON_ENABLE 3
-#define BUTTON_GPIO_PIN 39
+#define BUTTON_GPIO_PIN 1
 
 class audio_button {
 public:
     audio_button() {};
     void init();
-    void audio_activation(bool activated);
+    void audio_activation();
 private:
     void check_buttons();
     bool buttons[4] = {false, false, false, false};
     int voltages[4] = {29, 63, 153, 186};
+    bool activated;
 };
 
 #endif
